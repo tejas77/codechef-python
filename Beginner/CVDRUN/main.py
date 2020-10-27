8cd72741b3acbadd95d1eb8e38c7357d000be3e4
happytejasrathod <happytejasrathod@gmail.com>
2020-10-27 09:44:41 +0530

    Beginner - Covid Run

---
+++ Beginner/CVDRUN/main.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    n, k, x, y = map(int, input().split())
+    aCs = [x]
+    for _ in range(n):
+        aC = (k + x) % n
+        x = aC
+        if aC in aCs:
+            break
+        else:
+            aCs.append(aC)
+    print('YES' if y in aCs else 'NO')
