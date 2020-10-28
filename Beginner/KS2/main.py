9bb9e84c01afa3c15eb6da9d843a84294468bd30
happytejasrathod <happytejasrathod@gmail.com>
2020-10-28 22:53:46 +0530

    Beginner - Guddu on a Date Update

--- Beginner/KS2/main.py
+++ Beginner/KS2/main.py
@@ -2,6 +2,5 @@ from sys import stdin, stdout
 
 for _ in range(int(stdin.readline())):
     n = stdin.readline().strip()
-    nSum = sum(list(map(int, list(n))))
-    tenMinusSum = str(10 - ((nSum % 10) or 10))
-    stdout.write(f"{n + tenMinusSum}\n")
+    s = sum(map(int, n)) % 10
+    stdout.write(f"{n+str((10-s)%10)}\n")
