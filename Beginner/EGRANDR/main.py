cdeefce83023d363e0ec451659c5fac2ed542e91
happytejasrathod <happytejasrathod@gmail.com>
2020-11-07 10:59:26 +0530

    Beginner - Andrash and Stipendium

---
+++ Beginner/EGRANDR/main.py
@@ -0,0 +1,12 @@
+from sys import stdin, stdout
+ans = []
+
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    nArr = list(map(int, stdin.readline().split()))
+    print(sum(nArr))
+    if 2 not in nArr and 5 in nArr and sum(nArr) >= 4:
+        ans.append("Yes")
+    else:
+        ans.append("No")
+stdout.write('\n'.join(ans))
