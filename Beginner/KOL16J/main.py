e18351bbc572748aeb053f36663b72fa07c76398
happytejasrathod <happytejasrathod@gmail.com>
2020-11-29 01:38:13 +0530

    Beginner - Quentin Tarantino

---
+++ Beginner/KOL16J/main.py
@@ -0,0 +1,8 @@
+from sys import stdin, stdout
+ans = []
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    a = list(map(int, stdin.readline().split()))
+    b = [for i in range(1, n+1)]
+    ans.append('yes' if a != b and len(set(a)) == max(a) == n else 'no')
+stdout.write('\n'.join(ans))
